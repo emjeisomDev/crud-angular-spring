e35255d19e5be75c9f19a1bcb287ada174452251
emjeisomDev <emjeisom.dev@gmail.com>
2023-05-12 11:13:24 -0300

    Completed courses list in the front end

--- crud-angular/src/app/courses/courses/courses.component.ts
+++ crud-angular/src/app/courses/courses/courses.component.ts
@@ -2,7 +2,9 @@ import { Component, OnInit } from '@angular/core';
 
 import { Course } from '../model/course';
 import { CoursesService } from '../services/courses.service';
-import { Observable } from 'rxjs';
+import { Observable, catchError, of } from 'rxjs';
+import { MatDialog } from '@angular/material/dialog';
+import { ErrorDialogComponent } from 'src/app/shared/components/error-dialog/error-dialog.component';
 
 @Component({
   selector: 'app-courses',
@@ -12,11 +14,26 @@ import { Observable } from 'rxjs';
 export class CoursesComponent {
 
 
-  courses: Observable<Course[]>;
+  courses$: Observable<Course[]>;
   displayedColumns = ['name','category'];
 
-  constructor( private coursesService: CoursesService ){
-    this.courses = this.coursesService.list();
+  constructor(
+    private coursesService: CoursesService,
+    public dialog: MatDialog
+  ){
+    this.courses$ = this.coursesService.list()
+      .pipe(
+        catchError(error => {
+          this.onError('Erro ao carregar cursos!');
+          return of([]);
+        })
+      );
+  }
+
+  onError(errorMessage: string) {
+    this.dialog.open(ErrorDialogComponent, {
+      data: errorMessage,
+    });
   }
 
   ngOnInit(){
