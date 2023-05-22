25560c71b506d954303f2894a9aa76bb623c577f
emjeisomDev <emjeisom.dev@gmail.com>
2023-05-22 14:12:54 -0300

    Resolver: Search by ID

--- crud-angular/src/app/courses/container/courses/courses.component.ts
+++ crud-angular/src/app/courses/container/courses/courses.component.ts
@@ -41,5 +41,9 @@ export class CoursesComponent {
     this.router.navigate(['new'], {relativeTo: this.route});
   }
 
+  onEdit(course: Course){
+    this.router.navigate(['edit', course._id], {relativeTo: this.route});
+  }
+
 
 }
