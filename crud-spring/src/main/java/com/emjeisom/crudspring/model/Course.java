88ba97e81eb1c9dd600a7f03731fbf45be5ee9ec
emjeisomDev <emjeisom.dev@gmail.com>
2023-07-19 18:17:46 -0300

    API Validations with Java Bean Validation

--- crud-spring/src/main/java/com/emjeisom/crudspring/model/Course.java
+++ crud-spring/src/main/java/com/emjeisom/crudspring/model/Course.java
@@ -1,5 +1,7 @@
 package com.emjeisom.crudspring.model;
 
+import org.hibernate.validator.constraints.Length;
+
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import jakarta.persistence.Column;
@@ -7,6 +9,9 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
@@ -18,9 +23,16 @@ public class Course {
     @JsonProperty("_id")
     private Long id;
     
-    @Column(name = "name", length = 200, nullable = false)
+    @NotBlank
+    @NotNull
+    @Length(min=3, max = 100)
+    @Column(name = "name", length = 100, nullable = false)
     private String name;
 
+    @NotBlank
+    @NotNull
+    @Length(max = 10)
+    @Pattern(regexp = "back-end|front-end")
     @Column(name = "category", length = 20, nullable = false)
     private String category;
 
