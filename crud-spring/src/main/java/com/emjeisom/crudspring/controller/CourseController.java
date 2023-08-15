41b867294ae1335fffcce0e5c57b30a810218bf2
emjeisomDev <emjeisom.dev@gmail.com>
2023-08-15 14:01:11 -0300

    Added service layer

--- crud-spring/src/main/java/com/emjeisom/crudspring/controller/CourseController.java
+++ crud-spring/src/main/java/com/emjeisom/crudspring/controller/CourseController.java
@@ -12,65 +12,62 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.emjeisom.crudspring.model.Course;
-import com.emjeisom.crudspring.repository.CourseRepository;
+import com.emjeisom.crudspring.service.CourseService;
 
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
-import lombok.AllArgsConstructor;
 
 @Validated
 @RestController
 @RequestMapping("/api/courses")
-@AllArgsConstructor // O lombok cria o construtor através da anatação
 public class CourseController {
 
-    private final CourseRepository courseRepository;
+    private final CourseService courseService;
+
+    public CourseController(CourseService courseService) {
+        this.courseService = courseService;
+    }
 
     @GetMapping()
-    public List<Course> list(){
-        return courseRepository.findAll();
+    public @ResponseBody List<Course> list() {
+        return courseService.list();
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Course> findById(@PathVariable @NotNull @Positive Long id){
-        return courseRepository.findById(id)
-            .map(recordFound -> ResponseEntity.ok().body(recordFound))
-            .orElse(ResponseEntity.notFound().build());
+    public ResponseEntity<Course> findById(@PathVariable @NotNull @Positive Long id) {
+        return courseService.findById(id)
+                .map(recordFound -> ResponseEntity.ok().body(recordFound))
+                .orElse(ResponseEntity.notFound().build());
     }
 
     @PostMapping
     @ResponseStatus(code = HttpStatus.CREATED)
-    public Course create(@RequestBody @Valid Course course){
-        // return ResponseEntity.status(HttpStatus.CREATED)
-        //      body courseRepository save course 
-        return courseRepository.save(course);
+    public Course create(@RequestBody @Valid Course course) {
+        return courseService.create(course);
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Course> update(@PathVariable @Positive Long id, @RequestBody @Valid Course course){
-        return courseRepository.findById(id)
-                .map(recordFound -> {
-                    recordFound.setName(course.getName());
-                    recordFound.setCategory(course.getCategory());
-                    Course updated = courseRepository.save(recordFound);
-                    return ResponseEntity.ok().body(updated);
-                })
+    public ResponseEntity<Course> update(@PathVariable @Positive Long id, @RequestBody @Valid Course course) {
+        return courseService.update(id, course)
+                .map(recordFound ->ResponseEntity.ok().body(recordFound))
                 .orElse(ResponseEntity.notFound().build());
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> delete(@PathVariable @Positive Long id){
-        return courseRepository.findById(id)
-                .map(recordFound -> {
-                    courseRepository.deleteById(id);
-                    return ResponseEntity.noContent().<Void>build();
-                })
-                .orElse(ResponseEntity.notFound().build());
+    public ResponseEntity<Void> delete(@PathVariable @Positive Long id) {
+        if(courseService.delete(id)){
+            return ResponseEntity.noContent().<Void>build();
+        } 
+        else{
+            return ResponseEntity.notFound().<Void>build();
+        }
+            
     }
 
 }
